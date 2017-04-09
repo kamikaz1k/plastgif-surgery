bb422fef252992cbe161171914988a2787c518e6
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-04-09 14:58:45 -0400

    added resizing function

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -2,6 +2,14 @@ import cv2
 import sys
 import numpy as np
 
+WAIT = 1000
+
+def resize_face(x, y, w, h):
+    # resize face
+    small = cv2.resize(faceImg, (w, h))
+    cv2.imshow("resized face", small)
+    cv2.waitKey(WAIT)
+
 # Get user supplied values
 imagePath = sys.argv[1]
 cascPath = sys.argv[2]
@@ -24,11 +32,14 @@ faces = faceCascade.detectMultiScale(
 
 print("Found {0} faces!".format(len(faces)))
 
+faceImg = cv2.imread("face.png")
+
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    resize_face(x, y, w, h)
 
 cv2.imshow("Faces found", image)
-cv2.waitKey(2000)
+cv2.waitKey(WAIT)
 
 # Original from https://realpython.com/blog/python/face-recognition-with-python/
