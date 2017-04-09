418d08a2848413b022ca9c6ff016effe4500e746
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-04-09 16:09:16 -0400

    added face_swap function

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -1,14 +1,31 @@
 import cv2
 import sys
 import numpy as np
+import pdb
 
 WAIT = 1000
 
-def resize_face(x, y, w, h):
+def resize_face(w, h):
     # resize face
     small = cv2.resize(faceImg, (w, h))
-    cv2.imshow("resized face", small)
-    cv2.waitKey(WAIT)
+    # cv2.imshow("resized face", small)
+    # cv2.waitKey(WAIT)
+    return small
+
+def face_swap(face, img, xPos, yPos):
+    # Create a blank image with the face positioned in the correct location
+    blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
+    blank_image[yPos : yPos+face.shape[0], xPos: xPos+face.shape[1]] = face
+    # idea from http://stackoverflow.com/questions/12881926/create-a-new-rgb-opencv-image-using-python
+    
+    # Find locations of indices to slice
+    locs = np.where(blank_image != 0)
+    # idea from http://stackoverflow.com/questions/41572887/equivalent-of-copyto-in-python-opencv-bindings
+
+    # Slice the indices for the face location
+    img[locs[0], locs[1], locs[2]] = blank_image[locs[0], locs[1], locs[2]]
+    # cv2.imshow("Face swap", img)
+    # cv2.waitKey(WAIT)
 
 # Get user supplied values
 imagePath = sys.argv[1]
@@ -36,10 +53,11 @@ faceImg = cv2.imread("face.png")
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    resize_face(x, y, w, h)
+    # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    resized = resize_face(w, h)
+    face_swap(resized, image, x, y)
 
 cv2.imshow("Faces found", image)
-cv2.waitKey(WAIT)
+cv2.waitKey(0)
 
 # Original from https://realpython.com/blog/python/face-recognition-with-python/
