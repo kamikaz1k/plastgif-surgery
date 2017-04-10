8f34d3086d7aa8e011299f19aba0b4c3d5934cc8
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-04-09 22:26:35 -0400

    refactored the app logic to a function

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -6,7 +6,7 @@ import pdb
 WAIT = 1000
 DEBUG = False
 
-def resize_face(w, h):
+def resize_face(faceImg, w, h):
     # resize face
     small = cv2.resize(faceImg, (w, h))
     # cv2.imshow("resized face", small)
@@ -51,26 +51,15 @@ def face_swap(face, img, xPos, yPos):
     # cv2.imshow("Face swap", img)
     # cv2.waitKey(WAIT)
 
-if __name__ == "__main__":
-    '''Takes a source image and a face image as arguments. 
-    If the image has any faces, it will copy over it with the face image 
-
-    Arguments:
-    arg1 -- <source>
-    arg2 -- <face>
-    <source> image can be any format supported by opencv
-    <face> image can be any format support by opencv. 
-    If <face> is a PNG with an alpha channel, program will try to use to it
-    '''
-    # Get user supplied values
-    imagePath = sys.argv[1]
-    cascPath = "haarcascade_frontalface_default.xml" # sys.argv[2] 
+def find_and_replace(imagePath, facePath):
+    cascPath = "haarcascade_frontalface_default.xml" 
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
 
     # Read the image
     image = cv2.imread(imagePath)
+    faceImg = cv2.imread(facePath, -1)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the image
@@ -84,14 +73,32 @@ if __name__ == "__main__":
 
     print("Found {0} faces!".format(len(faces)))
 
-    # faceImg = cv2.imread("face.png", -1)
-    faceImg = cv2.imread(sys.argv[2], -1)
-
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        resized = resize_face(w, h)
+        resized = resize_face(faceImg, w, h)
         face_swap(resized, image, x, y)
+
+    return image
+
+if __name__ == "__main__":
+    '''Takes a source image and a face image as arguments. 
+    If the image has any faces, it will copy over it with the face image 
+
+    Arguments:
+    arg1 -- <source>
+    arg2 -- <face>
+    <source> image can be any format supported by opencv
+    <face> image can be any format support by opencv. 
+    If <face> is a PNG with an alpha channel, program will try to use to it
+    '''
+    # Get user supplied values
+    imagePath = sys.argv[1]
+    facePath = sys.argv[2]
+
+    print "arguments given", imagePath, facePath
+
+    image = find_and_replace(imagePath, facePath)
 
     cv2.imshow("Faces found", image)
     cv2.waitKey(0)
