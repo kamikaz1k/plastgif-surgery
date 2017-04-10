6bdcde6498bcacefdf919287fe00d4edfaa586a4
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-04-09 23:06:57 -0400

    added gif editing ability

--- face_detect_cv3.py
+++ face_detect_cv3.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import numpy as np
 import pdb
+import imageio
 
 WAIT = 1000
 DEBUG = False
@@ -51,15 +52,11 @@ def face_swap(face, img, xPos, yPos):
     # cv2.imshow("Face swap", img)
     # cv2.waitKey(WAIT)
 
-def find_and_replace(imagePath, facePath):
-    cascPath = "haarcascade_frontalface_default.xml" 
+def find_and_replace(image, face):
 
     # Create the haar cascade
+    cascPath = "haarcascade_frontalface_default.xml" 
     faceCascade = cv2.CascadeClassifier(cascPath)
-
-    # Read the image
-    image = cv2.imread(imagePath)
-    faceImg = cv2.imread(facePath, -1)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the image
@@ -76,7 +73,7 @@ def find_and_replace(imagePath, facePath):
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        resized = resize_face(faceImg, w, h)
+        resized = resize_face(face, w, h)
         face_swap(resized, image, x, y)
 
     return image
@@ -98,9 +95,31 @@ if __name__ == "__main__":
 
     print "arguments given", imagePath, facePath
 
-    image = find_and_replace(imagePath, facePath)
+    face = cv2.imread(facePath, -1)
 
-    cv2.imshow("Faces found", image)
-    cv2.waitKey(0)
+    # if gif
+    if imagePath.endswith("gif"):
+        images = imageio.mimread(imagePath)
+        output = []
+        for image in images:
+            # switch from RGB to BGR
+            r,g,b,a = cv2.split(image)
+            image = cv2.merge((b,g,r))
+            image = find_and_replace(image, face)
+            output.append(image)
+            if DEBUG:
+                cv2.imshow("Faces found", output[-1])
+                cv2.waitKey(0)
+        pdb.set_trace()
+        imageio.mimwrite("output.gif", output)
+
+    else:
+        # Read the image
+        image = cv2.imread(imagePath)
+
+        image = find_and_replace(image, face)
+
+        cv2.imshow("Faces found", image)
+        cv2.waitKey(0)
 
 # Original from https://realpython.com/blog/python/face-recognition-with-python/
